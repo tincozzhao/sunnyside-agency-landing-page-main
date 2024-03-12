b9dba6ac9073647b447a4aedfee86ecb50fe6667
tincozzhao <149267216+tincozzhao@users.noreply.github.com>
2024-03-11 17:24:12 -0700

    create final landing page and add JS button and prompt

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function helloName() {
+    let h1 = document.getElementById("WAC");
+    let name = prompt("What's Your Name?");
+    h1.innerHTML = "Hello " + name + ",<br /> We Are Creatives."
+}
+
